f6d90bc7450d5cbf685d0951bf0d222f7068825b
RebecaNavarro <130471557+RebecaNavarro@users.noreply.github.com>
2024-12-04 09:11:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,8 +27,8 @@ resource "aws_key_pair" "nginx-server-ssh" {
    public_key = file("nginx-server.key.pub")
 }
 
-resource "aws_security_group" "nginx-server-sg" {
- name        = "nginx-server-sg"
+resource "aws_security_group" "nginx-server2-sg" {
+ name        = "nginx-server2-sg"
  description = "Security group allowing SSH and HTTP access"
 
 
@@ -56,7 +56,7 @@ resource "aws_security_group" "nginx-server-sg" {
  }
 }
 
-resource "aws_instance" "nginx-server" {
+resource "aws_instance" "nginx-server2" {
  ami = "ami-0866a3c8686eaeeba"  # agrega el ami Amazon Linux de acuerdo a tu region
  instance_type = "t2.micro"
  #count = 2
@@ -78,6 +78,6 @@ resource "aws_instance" "nginx-server" {
              sudo systemctl start nginx
              EOF
  user_data_replace_on_change = true
- key_name = aws_key_pair.nginx-server-ssh.key_name
- vpc_security_group_ids = [ aws_security_group.nginx-server-sg.id ]
+ key_name = aws_key_pair.nginx-server2-ssh.key_name
+ vpc_security_group_ids = [ aws_security_group.nginx-server2-sg.id ]
 }
