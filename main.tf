6f62ae66be054b10aceabd8789eac0cdda0b6258
RebecaNavarro <130471557+RebecaNavarro@users.noreply.github.com>
2024-12-05 22:11:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,10 @@ resource "aws_instance" "nginx-server2" {
 
  tags = {
    Name = "Upb-Nginx"
+   Environment = "test"
+   Owner       = "becanavarro2003@gmail.com" # Puedes agregar tu mail para el tag
+   Team        = "DevOps"
+   Project     = "webinar"
  }
 
 
@@ -20,6 +24,8 @@ resource "aws_instance" "nginx-server2" {
              sudo systemctl start nginx
              EOF
  user_data_replace_on_change = true
+ key_name = aws_key_pair.nginx-server-ssh.key_name
+ vpc_security_group_ids = [ aws_security_group.nginx-server-sg.id ]
 }
 
 resource "aws_key_pair" "nginx-server-ssh" {
@@ -28,7 +34,7 @@ resource "aws_key_pair" "nginx-server-ssh" {
 }
 
 resource "aws_security_group" "nginx-server-sg" {
- name        = "nginx-server-sg2"
+ name        = "nginx-server-sg"
  description = "Security group allowing SSH and HTTP access"
 
 
@@ -54,30 +60,4 @@ resource "aws_security_group" "nginx-server-sg" {
    protocol    = "-1"
    cidr_blocks = ["0.0.0.0/0"]
  }
-}
-
-resource "aws_instance" "nginx-server3" {
- ami = "ami-0866a3c8686eaeeba"  # agrega el ami Amazon Linux de acuerdo a tu region
- instance_type = "t2.micro"
- #count = 2
-
-
- tags = {
-   Name        = "Upb-Nginx"
-   Environment = "test"
-   Owner       = "becanavarro2003@gmail.com" # Puedes agregar tu mail para el tag
-   Team        = "DevOps"
-   Project     = "webinar"
- }
-
-
- user_data = <<-EOF
-             #!/bin/bash
-             sudo yum install -y nginx
-             sudo systemctl enable nginx
-             sudo systemctl start nginx
-             EOF
- user_data_replace_on_change = true
- key_name = aws_key_pair.nginx-server-ssh.key_name
- vpc_security_group_ids = [ aws_security_group.nginx-server-sg.id ]
 }
